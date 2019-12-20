a319cae79f709fb56498cbf31602e53796856fb6
Peter Borden <pgborden@me.com>
2019-12-19 20:31:20 -0800

    First rev with three text fields and color and white buttons

--- RGBLamp/ViewController.swift
+++ RGBLamp/ViewController.swift
@@ -9,9 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var redColor: Float = 0.33
+    var greenColor: Float = 0.33
+    var blueColor: Float = 0.33
+    var whiteColor: Float = 0.90
+    
+    @IBOutlet weak var textLarge: UITextView!
+    @IBOutlet weak var textMedium: UITextView!
+    @IBOutlet weak var textSmall: UITextView!
+    
+    @IBAction func redSlider(_ slider: UISlider) {
+        redColor = slider.value
+        setBackgroundColor()
+    }
+    
+    @IBAction func greenSlider(_ slider: UISlider) {
+        greenColor = slider.value
+        setBackgroundColor()
+    }
+    
+    @IBAction func blueSlider(_ slider: UISlider) {
+        blueColor = slider.value
+        setBackgroundColor()
+    }
+    
+    @IBAction func whiteButton(_ button: UIButton) {
+        setBackgroundWhite()
+    }
+    
+    @IBAction func colorButton(_ button: UIButton) {
+        setBackgroundColor()
+    }
+    
+    func setBackgroundColor() {
+        textLarge.backgroundColor = UIColor(
+            red: CGFloat(redColor),
+            green: CGFloat(greenColor),
+            blue: CGFloat(blueColor),
+            alpha: 1.0
+        )
+        textMedium.backgroundColor = textLarge.backgroundColor
+        textSmall.backgroundColor = textLarge.backgroundColor
+    }
+    
+    func setBackgroundWhite() {
+        textLarge.backgroundColor = UIColor(
+            red: CGFloat(whiteColor),
+            green: CGFloat(whiteColor),
+            blue: CGFloat(whiteColor),
+            alpha: 1.0
+        )
+        textMedium.backgroundColor = textLarge.backgroundColor
+        textSmall.backgroundColor = textLarge.backgroundColor
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setBackgroundColor()
+        
         // Do any additional setup after loading the view.
     }
 
