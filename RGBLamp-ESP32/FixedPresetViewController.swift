6f28626ad85a31eb9a3524077d9a95770696623a
Peter Borden <pgborden@me.com>
2021-01-11 10:48:04 -0800

    01.11.21: Created two sections in presets, one for common settings and one for other settings (such as disease conditions)

--- RGBLamp-ESP32/FixedPresetViewController.swift
+++ RGBLamp-ESP32/FixedPresetViewController.swift
@@ -18,6 +18,19 @@ class FixedPresetViewController: UITableViewController {
     Preset(name: "Retinitis Pigmentosa", red: 0.9, green: 0.3, blue: 0.5, alpha: 0.9, editable: false, check: false),
     Preset(name: "Diabetic Retinopathy", red: 0.5, green: 0.9, blue: 0.4, alpha: 0.9, editable: false, check: false),
      ]
+    
+    var commonPresetArray = [Preset(name: "Cold white", red: 0.9, green: 0.9, blue: 1.0, alpha: 0.6, editable: false, check: false),
+                             Preset(name: "Neutral white", red: 0.9, green: 0.9, blue: 0.8, alpha: 0.6, editable: false, check: false),
+                             Preset(name: "Warm white", red: 0.9, green: 0.9, blue: 0.6, alpha: 0.6, editable: false, check: false)
+                              ]
+    
+    var conditionPresetArray = [Preset(name: "Macular Degeneration", red: 0.3, green: 1.0, blue: 0.5, alpha: 1.0, editable: false, check: false),
+                            Preset(name: "Glaucoma", red: 0.1, green: 0.9, blue: 0.9, alpha: 0.5, editable: false, check: false),
+    Preset(name: "Retinitis Pigmentosa", red: 0.9, green: 0.3, blue: 0.5, alpha: 0.9, editable: false, check: false),
+    Preset(name: "Diabetic Retinopathy", red: 0.5, green: 0.9, blue: 0.4, alpha: 0.9, editable: false, check: false)
+     ]
+    
+    var titleString = [0 : "StellaIQ™: Common Presets", 1 : "StellaIQ™: Other Presets"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,13 +43,139 @@ class FixedPresetViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
+    // see reference: https://stackoverflow.com/questions/48658391/how-to-make-multiple-level-sections-in-uitableview-in-swift-4
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return (commonPresetArray.count)
+        } else if section == 1 {
+            return (conditionPresetArray.count)
+        } else {
+            return 1
+        }
     }
     
-    // Create a standard header that includes the returned text.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PresetCell", for: indexPath)
+
+        // Configure the cell...
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        switch (indexPath.section) {
+            case 0:
+                let redBack = CGFloat(commonPresetArray[indexPath.row].red)
+                let greenBack = CGFloat(commonPresetArray[indexPath.row].green)
+                let blueBack = CGFloat(commonPresetArray[indexPath.row].blue)
+                let alphaBack = CGFloat(commonPresetArray[indexPath.row].alpha)
+                print("IndexPath \(indexPath.row), red \(redBack), green \(greenBack), blue \(blueBack)")
+                cell.backgroundColor = UIColor(red: redBack, green: greenBack, blue: blueBack, alpha: alphaBack)
+                cell.tintColor = UIColor.black
+                label.textColor = UIColor.black
+                label.text = commonPresetArray[indexPath.row].name
+            case 1:
+                let redBack = CGFloat(conditionPresetArray[indexPath.row].red)
+                let greenBack = CGFloat(conditionPresetArray[indexPath.row].green)
+                let blueBack = CGFloat(conditionPresetArray[indexPath.row].blue)
+                let alphaBack = CGFloat(conditionPresetArray[indexPath.row].alpha)
+                print("IndexPath \(indexPath.row), red \(redBack), green \(greenBack), blue \(blueBack)")
+                cell.backgroundColor = UIColor(red: redBack, green: greenBack, blue: blueBack, alpha: alphaBack)
+                cell.tintColor = UIColor.black
+                label.textColor = UIColor.black
+                label.text = conditionPresetArray[indexPath.row].name
+            default:
+                let redBack = CGFloat(conditionPresetArray[indexPath.row].red)
+                let greenBack = CGFloat(conditionPresetArray[indexPath.row].green)
+                let blueBack = CGFloat(conditionPresetArray[indexPath.row].blue)
+                let alphaBack = CGFloat(conditionPresetArray[indexPath.row].alpha)
+                print("IndexPath \(indexPath.row), red \(redBack), green \(greenBack), blue \(blueBack)")
+                cell.backgroundColor = UIColor(red: redBack, green: greenBack, blue: blueBack, alpha: alphaBack)
+                cell.tintColor = UIColor.black
+                label.textColor = UIColor.black
+                label.text = conditionPresetArray[indexPath.row].name
+        }
+
+        return cell
+        
+    }
+
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+
+        let sectionView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 50))
+        sectionView.backgroundColor = UIColor.black
+
+        let sectionName = UILabel(frame: CGRect(x: 5, y: 0, width: tableView.frame.size.width, height: 50))
+        sectionName.text = titleString[section]
+        sectionName.textColor = UIColor.white
+        sectionName.font = UIFont.systemFont(ofSize: 18)
+        sectionName.textAlignment = .left
+
+        sectionView.addSubview(sectionName)
+        return sectionView
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        for cell in tableView.visibleCells {
+            cell.accessoryType = .none
+        }
+        
+        // place a check mark on the selected row
+        if let cell = tableView.cellForRow(at: indexPath) {
+                cell.accessoryType = .checkmark
+            }
+        let section = indexPath.section
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        setLEDs(presetIndex: indexPath.row, section: Int64(section))
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func setLEDs(presetIndex: Int, section: Int64) {
+        var redColor: Float = 0.0
+        var greenColor: Float = 0.0
+        var blueColor: Float = 0.0
+        var alpha: Float = 0.0
+        if section == 0 {
+            redColor = Float(commonPresetArray[presetIndex].red)
+            greenColor = Float(commonPresetArray[presetIndex].green)
+            blueColor = Float(commonPresetArray[presetIndex].blue)
+            alpha = Float(commonPresetArray[presetIndex].alpha)
+        }
+        if section == 1 {
+            redColor = Float(conditionPresetArray[presetIndex].red)
+            greenColor = Float(conditionPresetArray[presetIndex].green)
+            blueColor = Float(conditionPresetArray[presetIndex].blue)
+            alpha = Float(conditionPresetArray[presetIndex].alpha)
+        }
+        
+        // convert to LED values using routine in ADCMaxValue.swift
+        let redLED = ledValue(color: "red", for: redColor, for: greenColor, for: blueColor, for: alpha)
+        let greenLED = ledValue(color: "green", for: redColor, for: greenColor, for: blueColor, for: alpha)
+        let blueLED = ledValue(color: "blue", for: redColor, for: greenColor, for: blueColor, for: alpha)
+        let whiteLED: Int = 0
+        
+        saveValues(for: redColor, for: greenColor, for: blueColor, for: alpha)
+        
+        valueToString(white: whiteLED, red: redLED, green: greenLED, blue: blueLED)
+    }
+    
+    
+    
+   /* // Create a standard header that includes the returned text.
+     
+     /*override func numberOfSections(in tableView: UITableView) -> Int {
+         // #warning Incomplete implementation, return the number of sections
+         return 1
+     } */
+     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection
                                 section: Int) -> String? {
        return "StellaIQ™ Presets"
@@ -88,24 +227,9 @@ class FixedPresetViewController: UITableViewController {
 
         return cell
     }
-    
-    func setLEDs(presetIndex: Int) {
-        
-        let redColor = Float(fixedPresetArray[presetIndex].red)
-        let greenColor = Float(fixedPresetArray[presetIndex].green)
-        let blueColor = Float(fixedPresetArray[presetIndex].blue)
-        let alpha = Float(fixedPresetArray[presetIndex].alpha)
-        
-        // convert to LED values using routine in ADCMaxValue.swift
-        let redLED = ledValue(color: "red", for: redColor, for: greenColor, for: blueColor, for: alpha)
-        let greenLED = ledValue(color: "green", for: redColor, for: greenColor, for: blueColor, for: alpha)
-        let blueLED = ledValue(color: "blue", for: redColor, for: greenColor, for: blueColor, for: alpha)
-        let whiteLED: Int = 0
-        
-        saveValues(for: redColor, for: greenColor, for: blueColor, for: alpha)
-        
-        valueToString(white: whiteLED, red: redLED, green: greenLED, blue: blueLED)
-    }
+ 
+ */
+   
     
 
     /*
